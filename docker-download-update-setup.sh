787f8a3f3d70452ae5dde029348584c0a6e08398
phouverneyuff <ph_ouverney@id.uff.br>
2015-07-01 21:09:42 -0300

    Improving...

--- docker-download-update-setup.sh
+++ docker-download-update-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Efetuando o download do docker do JHipster ..."
-sudo docker pull jdubois/jhipster-docker:latest >> /dev/null
+sudo docker pull jdubois/jhipster-docker:latest
 
 echo "Efetuando o download do docker do MySQL ..."
-sudo docker pull mysql/mysql-server:latest  >> /dev/null
+sudo docker pull mysql/mysql-server:latest
 
 MYSQL_DATABASE=pedido
 MYSQL_ROOT_PASSWORD=rootpwd
@@ -13,6 +13,8 @@ MYSQL_PASSWORD=pedidopwd
 
 MYSQL_VAR=mysql-var
 VOLUME_MYSQL_VAR=`pwd`/$MYSQL_VAR
+
+VOLUME_JHIPSTER=$1
 
 if [ ! -d $MYSQL_VAR ]; then
 	mkdir $MYSQL_VAR
@@ -33,14 +35,12 @@ echo "Service Name to Mysql: 'mysql'"
 echo "###################################################################################################"
 echo ""
 
-echo "Iniciando pela primeira vez o serviço do MySQL. Para iniciar da proxima vez: 'docker start msqyl'"
-sudo docker rm -f mysql >> /dev/null
-sudo docker run -v $VOLUME_MYSQL_VAR:/var/lib/mysql -p 3306:3306 -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_PASSWORD -d --name mysql mysql/mysql-server
+echo "Iniciando pela primeira vez o serviço do MySQL. Para iniciar da proxima vez: 'docker start mysql'"
+sudo docker rm -f mysql
+sudo docker run -v $VOLUME_MYSQL_VAR:/var/lib/mysql -P -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_PASSWORD -d --name mysql mysql/mysql-server
 
 echo ""
 echo "###################################################################################################"
-
-VOLUME_JHIPSTER=`pwd`/../codigo/app
 
 echo "Volume compartilhado do JHipster: $VOLUME_JHIPSTER"
 
@@ -48,7 +48,7 @@ echo "##########################################################################
 echo ""
 
 echo "Iniciando pela primeira vez o serviço do JHipster. Para iniciar da proxima vez: 'docker start jhipster'" 
-sudo docker rm -f jhipster >> /dev/null
+sudo docker rm -f jhipster
 sudo docker run --name jhipster --link mysql:mysql -v $VOLUME_JHIPSTER:/jhipster -p 8081:8080 -p 3000:3000 -p 3001:3000 -p 4022:22 -d -t -i jdubois/jhipster-docker /bin/bash
 
 echo ""
